a69f45832250c34eb335dcd7d6db317249dafddb
Andy Lian <andylian@silverlakeaxis.com>
2022-08-04 16:22:43 +0800

    Initial commit

---
+++ src/main/java/com/github/andylke/demo/demojenkinspipelinemavenbuild/DemoJenkinsPipelineSpringBoot.java
@@ -0,0 +1,13 @@
+package com.github.andylke.demo.demojenkinspipelinemavenbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsPipelineSpringBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsPipelineSpringBoot.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/andylke/demo/demojenkinspipelinemavenbuild/DemoJenkinsPipelineSpringBootTests.java
@@ -0,0 +1,13 @@
+package com.github.andylke.demo.demojenkinspipelinemavenbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsPipelineSpringBootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
